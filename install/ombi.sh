e061608a8116946988567001de0ea8e05af95f9f
sjdaniel10 <sjdaniel6241@gmail.com>
2018-05-19 23:42:58 +0200

    Fixed Ombi Download
    Ombi script was downloading all the ombi releases for all the platfroms

--- install/ombi.sh
+++ install/ombi.sh
@@ -1,5 +1,5 @@
 sudo adduser --disabled-password --system --no-create-home --gecos "Ombi Service" --group ombi
-sudo wget -P /opt $( curl -s https://api.github.com/repos/tidusjar/Ombi/releases/latest | grep browser_download_url | cut -d \" -f 4 )
+sudo wget -P /opt $( curl -s https://api.github.com/repos/tidusjar/Ombi/releases/latest | grep -E browser_download_url | grep -E linux.tar | cut -d \" -f 4 )
 sudo mkdir /opt/ombi
 sudo unzip /opt/Ombi.zip -d /opt/ombi
 sudo rm -rf /opt/Ombi.zip
@@ -7,7 +7,7 @@ sudo chown -R ombi:ombi /opt/ombi
 sudo echo "[Unit]
 Description=Ombi Service
 After=network.target
- 
+
 [Service]
 User=ombi
 Group=ombi
@@ -16,7 +16,7 @@ WorkingDirectory=/opt/ombi/Release/
 ExecStart=/usr/bin/mono /opt/ombi/Release/Ombi.exe
 Restart=always
 TimeoutStopSec=30
- 
+
 [Install]
 WantedBy=multi-user.target"> /lib/systemd/system/ombi.service
 
