066be046f2a6393fc32b6a1324c31451cadb7029
sjdaniel10 <sjdaniel6241@gmail.com>
2018-05-20 00:47:22 +0200

    Fixed Prep script
    Forgot to add some shit to be installed before the setups begin

--- install/prep.sh
+++ install/prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #installing prerequisites
-sudo apt-get install -y python2.7 mono-complete git-core python-lxml python-pip unzip libcurl4-openssl-dev bzip2 git
+sudo apt-get install -y python2.7 mono-complete git-core python-lxml python-pip unzip libcurl4-openssl-dev bzip2 git build-essential unzip
 
 #creating main data location
 sudo mkdir /opt/ProgramData/
