b6ed3d3c9007fe2c789bdb24451cad6d8d22b697
sjdaniel10 <sjdaniel6241@gmail.com>
2018-05-20 02:48:19 +0200

    Changed default download location: Ombi
    wget was just messing with me

--- install/ombi.sh
+++ install/ombi.sh
@@ -1,9 +1,8 @@
 sudo adduser --disabled-password --system --no-create-home --gecos "Ombi Service" --group ombi
-sudo wget -t 0 -T 15 -P /opt -O Ombi.tar.gz $( curl -s https://api.github.com/repos/tidusjar/Ombi/releases/latest | grep -E browser_download_url | grep -E linux.tar | cut -d \" -f 4 )
 sudo mkdir /opt/ombi
+sudo wget -t 0 -T 15 -O /opt/Ombi.tar.gz $( curl -s https://api.github.com/repos/tidusjar/Ombi/releases/latest | grep -E browser_download_url | grep -E linux.tar | cut -d \" -f 4 )
 tar xvfz /opt/Ombi.tar.gz -C /opt/ombi
-#sudo unzip /opt/Ombi.zip -d /opt/ombi
-sudo rm -rf /opt/Ombi.zip
+sudo rm -rf /opt/Ombi.tar.gz
 sudo chown -R ombi:ombi /opt/ombi
 sudo echo "[Unit]
 Description=Ombi Service
