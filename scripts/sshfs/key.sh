2e108cc732bd5330329e35bcdc7c13230fb7d8ca
sjdaniel10 <sjdaniel6241@gmail.com>
2018-07-09 14:14:53 +0200

    added nginx modules
    only added reverse proxy support for sonarr, radarr and jackett

--- scripts/sshfs/key.sh
+++ scripts/sshfs/key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -r $HOME/.ssh/id_ecdsa ]; then ssh-keygen -b 521 -t ecdsa; fi
-if [ $port != 22];
+if [ $sshport != 22];
     then ssh-copy-id $user@$servername;
 else
-ssh-copy-id $user@$servername:$port;
+ssh-copy-id $user@$servername:$sshport;
 fi
