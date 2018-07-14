879617091d838b4d2a00bf0c44aa24627522c908
sjdaniel10 <sjdaniel6241@gmail.com>
2018-07-14 21:05:00 +0200

    added curl as a requisite
    rip

--- scripts/install/prep.sh
+++ scripts/install/prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #installing prerequisites
-sudo apt-get install -y python2.7 mono-complete git-core python-lxml python-pip unzip libcurl4-openssl-dev bzip2 git build-essential unzip libicu-dev libunwind8 libcurl4-openssl-dev
+sudo apt-get install -y python2.7 mono-complete git-core python-lxml python-pip unzip libcurl4-openssl-dev bzip2 git build-essential unzip libicu-dev libunwind8 libcurl4-openssl-dev curl
 
 #creating main data location
 sudo mkdir /opt/ProgramData/
