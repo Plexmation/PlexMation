8fd1fcbcd9db9f9ab8afe7eaee499e9f265dcf61
sjdaniel10 <sjdaniel6241@gmail.com>
2018-10-08 01:00:16 +0200

    radarr location changes

--- scripts/install/radarr.sh
+++ scripts/install/radarr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo adduser --disabled-password --system --home /opt/ProgramData/radarr --gecos "Radarr Service" --group radarr
+sudo adduser --disabled-password --system --home /opt/ProgramData/Radarr --gecos "Radarr Service" --group radarr
 sudo apt update -y && sudo apt install -y libmono-cil-dev mediainfo
 #cd /opt
 wget -t 0 -T 15 -P /opt/ProgramData $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
-tar -xvzf /opt/ProgramData/Radarr.develop.*.linux.tar.gz -C /opt
+tar -xvzf /opt/ProgramData/Radarr.develop.*.linux.tar.gz -C /opt/ProgramData
 rm -rf /opt/ProgramData/Radarr.develop.*.linux.tar.gz
 sudo chown -R radarr:radarr /opt/ProgramData/Radarr
 sudo echo "[Unit]
