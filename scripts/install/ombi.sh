98943c60fbd4034a219ecb7cad6d8e72d59e1fa2
sjdaniel10 <sjdaniel6241@gmail.com>
2018-11-30 21:06:58 +0200

    changed ombi download location
    changed the temporary download and extract location for the ombi install script

--- scripts/install/ombi.sh
+++ scripts/install/ombi.sh
@@ -2,7 +2,7 @@
 sudo adduser --disabled-password --system --no-create-home --gecos "Ombi Service" --group ombi
 sudo mkdir /opt/ProgramData/ombi
 sudo wget -t 0 -T 15 -O /opt/ProgramData/Ombi.tar.gz $( curl -s https://api.github.com/repos/tidusjar/Ombi/releases/latest | grep -E browser_download_url | grep -E linux.tar | cut -d \" -f 4 )
-sudo tar xvfz /opt/Ombi.tar.gz -C /opt/ProgramData/ombi
+sudo tar xvfz /opt/ProgramData/Ombi.tar.gz -C /opt/ProgramData/ombi
 sudo rm -rf /opt/ProgramData/Ombi.tar.gz
 sudo chown -R ombi:ombi /opt/ProgramData/ombi
 sudo echo "[Unit]
