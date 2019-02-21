f3996ed3b5a2a9604340a8cf6ab7e103031940f5
sjdaniel10 <sjdaniel6241@gmail.com>
2019-02-21 14:46:23 +0200

    Update radarr.sh
    && sign causing problems

--- scripts/install/radarr.sh
+++ scripts/install/radarr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo adduser --disabled-password --system --home /opt/ProgramData/Radarr --gecos "Radarr Service" --group radarr
-sudo apt update -y && sudo apt install -y libmono-cil-dev mediainfo
+sudo apt update -y
+sudo apt install -y libmono-cil-dev mediainfo
 #cd /opt
 wget -t 0 -T 15 -P /opt/ProgramData $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
 tar -xvzf /opt/ProgramData/Radarr.*.linux.tar.gz -C /opt/ProgramData
