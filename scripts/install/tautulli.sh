9d42aa325eda56aa9a784fdaa2dc3625552c8005
sjdaniel10 <sjdaniel6241@gmail.com>
2019-02-21 10:40:32 +0200

    Create tautulli.sh

---
+++ scripts/install/tautulli.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#install dependencies
+sudo apt-get install python python-setuptools tzdata
+
+#adding user and group
+sudo addgroup tautulli && sudo adduser --system --no-create-home tautulli --ingroup tautulli
+sudo adduser --system --no-create-home tautulli
+
+#cloning repo
+cd /opt
+sudo git clone https://github.com/Tautulli/Tautulli.git
+
+#permissions
+sudo chown tautulli:tautulli -R /opt/Tautulli
+
+#creating daemon scripts
+echo echo "[Unit]
+Description=Tautulli - Stats for Plex Media Server usage
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/opt/Tautulli/Tautulli.py --config /opt/Tautulli/config.ini --datadir /opt/Tautulli --quiet --daemon --nolaunch
+GuessMainPID=no
+Type=forking
+User=tautulli
+Group=tautulli
+
+[Install]
+WantedBy=multi-user.target"> /lib/systemd/system/tautulli.service
+
+sudo systemctl enable tautulli
+sudo systemctl start tautulli
